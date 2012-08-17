64166b1626b0a3a253a085ca107d1ade92f3d601
Jeremy Roberts <robertsj@mit.edu>
2012-08-17 12:48:10 -0400

    Modified CMake SLEPc finder

--- tools/CMake/FindSLEPc.cmake
+++ tools/CMake/FindSLEPc.cmake
@@ -1,23 +1,25 @@
-
+set(SLEPC_DIR $ENV{SLEPC_DIR})
 message("*** SLEPC_DIR = ${SLEPC_DIR}")
- 
-SET(SLEPC_INCLUDE_DIR_A "${SLEPC_DIR}/${SLEPC_ARCH}/include")
-SET(SLEPC_INCLUDE_DIR   "${SLEPC_DIR}/include")
-SET(SLEPC_LIB_DIR       "${SLEPC_DIR}/${SLEPC_ARCH}/lib")
 
-FIND_LIBRARY(SLEPC_LIB_SLEPC     slepc 
-             HINTS ${SLEPC_DIR}/${SLEPC_ARCH}/lib)
 
-message( "*** SLEPc directory : ${SLEPC_DIR} ${SLEPC_LIB_SLEPC}"  )
+set(SLEPC_INCLUDE_DIR_A "${SLEPC_DIR}/${PETSC_ARCH}/include")
+set(SLEPC_INCLUDE_DIR   "${SLEPC_DIR}/include")
+set(SLEPC_LIB_DIR       "${SLEPC_DIR}/${PETSC_ARCH}/lib")
+
+message(" lib dir = ${SLEPC_DIR}/${PETSC_ARCH}/lib")
+find_library(SLEPC_LIB_SLEPC     slepc 
+             HINTS ${SLEPC_DIR}/${PETSC_ARCH}/lib)
+
+message(${SLEPC_LIB_SLEPC})
 
 if (SLEPC_DIR AND SLEPC_LIB_SLEPC )
-  SET(SLEPC_LIBRARIES ${SLEPC_LIB_SLEPC} CACHE STRING "SLEPc libraries" FORCE)
+  set(SLEPC_LIBRARIES ${SLEPC_LIB_SLEPC} CACHE STRING "SLEPc libraries" FORCE)
   set(HAVE_SLEPC 1)
   set(SLEPC_FOUND ON)
   message( "-- Found SLEPc: ${SLEPC_LIBRARIES}" )
   set(SLEPC_INCLUDES ${SLEPC_INCLUDE_DIR} ${SLEPC_INCLUDE_DIR_A} CACHE STRING "SLEPc include path" FORCE)
   mark_as_advanced( SLEPC_DIR SLEPC_LIB_SLEPC SLEPC_INCLUDES SLEPC_LIBRARIES )
 else()
-  message( "-- SLEPc not found!" )
+  message(FATAL_ERROR "SLEPc not found!" )
 endif()
 
